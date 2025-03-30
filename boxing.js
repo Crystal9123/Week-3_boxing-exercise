52ec6a9a42b6ec3375c18e046eb212048c28a803
Crystal9123 <crystalcar98@gmail.com>
2025-03-30 23:12:00 +0000

    added changes to .js

---
+++ boxing.js
@@ -0,0 +1,12 @@
+let player1 = {
+    name: "Player 1",
+    health: 100
+};  
+    
+let player2 = {
+    name: "Player2",
+    health: 100
+};
+
+let currentPlayer = player1;
+console.log("current player:", currentPlayer.name)// Output: Current Player: Player 1
