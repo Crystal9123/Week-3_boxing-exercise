c7a77b788dbbd839f2d88f8c2b7305439e413c1d
Crystal9123 <crystalcar98@gmail.com>
2025-03-30 23:25:16 -0400

    Update boxing.js

--- boxing.js
+++ boxing.js
@@ -14,15 +14,15 @@ console.log("current player:", currentPlayer.name)// Output: Current Player: Pla
 
 // Function to apply damage to the opponent
 function applyDamage(opponent, damage) {
-if (/* FILL CONDITIONAL HERE */) {
+if (damage >= 10 && damage < 20) {
 console.log(`${currentPlayer.name} lands a basic hit on ${opponent.name}!`);
-} else if (/* FILL CONDITIONAL HERE */) {
+} else if ((damage >= 20 && damage < 30) {
 console.log(`${currentPlayer.name} lands a critical hit on ${opponent.name}!`);
 } else {
 console.log(`${currentPlayer.name} lands a KNOCKOUT on ${opponent.name}!🥊`);
 }
 opponent.health -= damage;
-if (opponent.health < 0 || /* FILL CONDITIONAL HERE */) {
+if (opponent.health < 0 || opponent.health === 0) {
 // Ensure health doesn't go below 0 and automatic KO
 opponent.health = 0;
 }
